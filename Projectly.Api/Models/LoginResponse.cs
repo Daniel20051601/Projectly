e0b3863fa9f65ca9c57ed2fbf6897c87c27e2775
Danielvi <danielvilopez@gmail.com>
2025-12-23 22:50:11 -0400

    feat: ✨Agrega los modelos para manejar jwt en la Api

---
+++ Projectly.Api/Models/LoginResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace Projectly.Api.Models;
+
+public class LoginResponse
+{
+    public string Token { get; set; } = string.Empty;
+    public string UsuarioId { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Nombre { get; set; } = string.Empty;
+    public bool IsPremium { get; set; }
+    public DateTime ExpiresAt { get; set; }
+}
